d625605e4c142df88ec1e4d48d41d675269a3e17
gvheisler <gvheisler@inf.ufsm.br>
2024-03-28 17:31:50 -0300

    update

--- tdd/numeros-romanos/app/src/main/java/org/example/ConversorNumeros.java
+++ tdd/numeros-romanos/app/src/main/java/org/example/ConversorNumeros.java
@@ -3,9 +3,24 @@
  */
 package org.example;
 
+import java.util.HashMap;
+
 public class ConversorNumeros {
+	private static HashMap<Character, Integer> tabela = new HashMap<Character, Integer>(){{
+		put('I', 1);
+		put('V', 5);
+		put('X', 10);
+		put('L', 50);
+		put('C', 100);
+		put('D', 500);
+		put('M', 1000);
+	}};
     public static int converteRtoI(String num_romano) {
-    	num_romano.toLowerCase();
-    	return 0;
+    	num_romano = num_romano.toUpperCase();
+    	int acum = 0;
+    	for(int i = 0; i < num_romano.length(); i++) {
+    		acum += tabela.get(num_romano.charAt(i));
+    	}
+    	return acum;
     }
 }
--- tdd/numeros-romanos/app/src/test/java/org/example/ConversorTest.java
+++ tdd/numeros-romanos/app/src/test/java/org/example/ConversorTest.java
@@ -5,12 +5,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ConversorTest {
     @Test 
-    void testaX(){
-    	assertEquals(10, ConversorNumeros.converteRtoI("x"));
+    void testaI(){
+    	assertEquals(1, ConversorNumeros.converteRtoI("I"));
     }
     
-    void testaIX(){
-    	assertEquals(9, ConversorNumeros.converteRtoI("ix"));
+    @Test 
+    void testaV(){
+    	assertEquals(5, ConversorNumeros.converteRtoI("V"));
+    }
+    
+    @Test 
+    void testaC(){
+    	assertEquals(100, ConversorNumeros.converteRtoI("C"));
+    }
+    
+    @Test 
+    void testaII() {
+    	assertEquals(2, ConversorNumeros.converteRtoI("II"));
+    }
+    
+    @Test 
+    void testaXXII() {
+    	assertEquals(22, ConversorNumeros.converteRtoI("XXII"));
+    }
+    
+    @Test 
+    void testaIX() {
     	assertEquals(9, ConversorNumeros.converteRtoI("IX"));
     }
 }
